09f1daf64a15f863f63f27d52798f4dc5497bba8
Jens Schulze <jenschude@gmail.com>
2020-10-26 20:53:47 +0100

    add custom ClientFactory for diferent flows

--- src/main/java/handson/Task09b_SPHERECLIENT_LOGGING.java
+++ src/main/java/handson/Task09b_SPHERECLIENT_LOGGING.java
@@ -6,8 +6,15 @@ import com.commercetools.api.defaultconfig.ApiFactory;
 import com.commercetools.api.defaultconfig.ServiceRegion;
 import handson.impl.ClientService;
 import io.vrap.rmf.base.client.ApiHttpHeaders;
+import io.vrap.rmf.base.client.AuthenticationToken;
+import io.vrap.rmf.base.client.ClientFactory;
+import io.vrap.rmf.base.client.VrapHttpClient;
 import io.vrap.rmf.base.client.http.RetryMiddleware;
+import io.vrap.rmf.base.client.oauth2.AnonymousSessionTokenSupplier;
 import io.vrap.rmf.base.client.oauth2.ClientCredentials;
+import io.vrap.rmf.base.client.oauth2.GlobalCustomerPasswordTokenSupplier;
+import io.vrap.rmf.base.client.oauth2.StaticTokenSupplier;
+import io.vrap.rmf.okhttp.VrapOkHttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -71,5 +78,66 @@ public class Task09b_SPHERECLIENT_LOGGING {
                         new RetryMiddleware(3, Arrays.asList(500, 503))
                 )
         );
+    }
+
+    static class ClientFactory {
+        public static ApiRoot createStatic(
+                final String token,
+                final String apiEndpoint
+        ) {
+            AuthenticationToken t = new AuthenticationToken();
+            t.setAccessToken(token);
+            return ApiRoot.fromClient(
+                    io.vrap.rmf.base.client.ClientFactory.create(
+                            apiEndpoint,
+                            new VrapOkHttpClient(),
+                            new StaticTokenSupplier(t)
+                    )
+            );
+        }
+
+        public static ApiRoot createAnonFlow(
+                final ClientCredentials credentials,
+                final String tokenEndpoint,
+                final String apiEndpoint
+        ) {
+            return ApiRoot.fromClient(
+                    io.vrap.rmf.base.client.ClientFactory.create(
+                            apiEndpoint,
+                            new VrapOkHttpClient(),
+                            new AnonymousSessionTokenSupplier(
+                                    credentials.getClientId(),
+                                    credentials.getClientSecret(),
+                                    credentials.getScopes(),
+                                    tokenEndpoint,
+                                    new VrapOkHttpClient()
+                            )
+                    )
+            );
+        }
+
+        public static ApiRoot createPasswordFlow(
+                final String userEmail,
+                final String userPassword,
+                final ClientCredentials credentials,
+                final String tokenEndpoint,
+                final String apiEndpoint
+        ) {
+            return ApiRoot.fromClient(
+                    io.vrap.rmf.base.client.ClientFactory.create(
+                            apiEndpoint,
+                            new VrapOkHttpClient(),
+                            new GlobalCustomerPasswordTokenSupplier(
+                                    credentials.getClientId(),
+                                    credentials.getClientSecret(),
+                                    userEmail,
+                                    userPassword,
+                                    credentials.getScopes(),
+                                    tokenEndpoint,
+                                    new VrapOkHttpClient()
+                            )
+                    )
+            );
+        }
     }
 }
