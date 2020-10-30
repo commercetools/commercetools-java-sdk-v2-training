f9161058cc169b8d133744038821cff4496b40ba
DESKTOP-BBJ59GQ\micha <Michael.Jua.Hartwig@gmail.com>
2020-10-30 07:42:21 +0100

    Task09a_ERROR_HANDLING Minor changes to streamline look according to all other tasks

--- src/main/java/handson/Task09a_ERROR_HANDLING.java
+++ src/main/java/handson/Task09a_ERROR_HANDLING.java
@@ -27,33 +27,43 @@ public class Task09a_ERROR_HANDLING {
 
     public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
 
-        final String projectKey = getProjectKey("mh-dev-admin.");
-        final ApiRoot client = createApiClient("mh-dev-admin.");
+        // TODO:
+        //  Check your prefix
+        //
+        String apiClientPrefix = "mh-dev-admin.";
+        final String projectKey = getProjectKey(apiClientPrefix);
+        final ApiRoot client = createApiClient(apiClientPrefix);
         Logger logger = LoggerFactory.getLogger(Task09a_ERROR_HANDLING.class.getName());
 
         try (ApiHttpClient apiHttpClient = ClientService.apiHttpClient) {
             CustomerService customerService = new CustomerService(client, projectKey);
 
+            // TODO:
+            //  Provide a WRONG or CORRECT customer key
+            //
+            final String customerKeyMayOrMayNotExist = "customer-michele-WRONG-KEY";
 
-            // Handle 4XX errors
-
-
-            // TODO: Handle exceptions, CompletionStage
+            // TODO: Handle 4XX errors, exceptions
+            //  Use CompletionStage
             //
             logger.info("Customer fetch: " +
                     customerService
-                            .getCustomerByKey("customer-michele-WRONG-KEY")
+                            .getCustomerByKey(customerKeyMayOrMayNotExist)
                             .thenApply(ApiHttpResponse::getBody) // unpack response body
                             .exceptionally(throwable -> {
-                                logger.info("I am not existing");
+                                logger.info("Customer " + customerKeyMayOrMayNotExist + " does not exist.");
                                 // handle it
-                                return CustomerBuilder.of().email("anonymous@example.org").build(); // e.g. return anon customer
+                                return
+                                        CustomerBuilder.of()
+                                                .email("anonymous@example.org")
+                                                .build();                               // e.g. return anon customer
                             })
                             .toCompletableFuture().get().getEmail()
             );
 
 
-            // TODO: Handle exceptions, Optionals, Either (Java 9+)
+            // TODO: Handle 4XX errors, exceptions
+            //  Use Optionals, Either (Java 9+)
             //
             Optional<Customer> optionalCustomer = Optional.ofNullable(
                     customerService
@@ -64,12 +74,12 @@ public class Task09a_ERROR_HANDLING {
             );
 
             if (!optionalCustomer.isPresent()) {
-                logger.info("I am not existing");
-                // handle it
+                logger.info("Customer " + customerKeyMayOrMayNotExist + " does not exist.");
+                // handle it, return anon customer, etc.
             }
 
             optionalCustomer.ifPresent(customer -> {
-                logger.info("I know that I exist.");
+                logger.info("Customer: " + customerKeyMayOrMayNotExist + "exists.");
                 try {
                     customerService.createEmailVerificationToken(customer, 5)
                             .thenComposeAsync(customerTokenApiHttpResponse -> customerService.verifyEmail(
